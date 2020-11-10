d72bc946ff125d3448bae2ba99df087ec5ec0d3a
unknown <ali.e7790@gmail.com>
2020-11-10 19:06:37 +0330

    Updates

---
+++ precache-manifest.68b0836da994552b54ffcd9b724fcf3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e3fc6a079ef295232b77355dd414aa",
+    "url": "/My-resume/index.html"
+  },
+  {
+    "revision": "9d3dd16fd33dfb4492db",
+    "url": "/My-resume/static/css/main.8ea72014.chunk.css"
+  },
+  {
+    "revision": "b8fb9c68303a4b325d31",
+    "url": "/My-resume/static/js/2.a921690f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/My-resume/static/js/2.a921690f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3dd16fd33dfb4492db",
+    "url": "/My-resume/static/js/main.3fd9f897.chunk.js"
+  },
+  {
+    "revision": "ea4c33b49798a90128ff",
+    "url": "/My-resume/static/js/runtime-main.ee76e383.js"
+  },
+  {
+    "revision": "9f9de2851bcfe7f51e86185c91309305",
+    "url": "/My-resume/static/media/css.9f9de285.png"
+  },
+  {
+    "revision": "3d380d66f1701ab615ea3c159a610456",
+    "url": "/My-resume/static/media/java.3d380d66.png"
+  },
+  {
+    "revision": "88b5e0ec0c5bdd59e7a1362add84bb57",
+    "url": "/My-resume/static/media/react.88b5e0ec.png"
+  },
+  {
+    "revision": "fa1ded1ed7c11438a9b0385b1e112850",
+    "url": "/My-resume/static/media/robots.fa1ded1e.txt"
+  }
+]);
